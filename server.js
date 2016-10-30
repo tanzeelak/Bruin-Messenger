22a54122f14dd77d97bdd1909a26f5f3c71ac68b
David Chen <David Chen>
2016-10-30 12:06:29 -0700

    made login thing

--- server.js
+++ server.js
@@ -8,10 +8,27 @@ app.use(express.static('public'))
 app.set('view engine', 'hbs')
 
 // TODO: Serve up the login page
-app.get('/', (req, res, next) => {
+//app.get('/', (req, res, next) => {
     // TODO
-})
+//})
+let user = null;
+app.get('/', (req, res) => {
+	if (!user)
+		return res.redirect('/login.html');
+	return res.redirect('/library');
+});
 
+/**
+ * Handle POST request from the login form (note, this is different than
+ * showing the login form, which is done by sending back public/login.html).
+ * Set the global `user` variable to whatever was sent to us by the form
+ * (the attribute "name" had value "name", so the information was put into
+ * `res.body.name` by the body parser)
+ */
+app.post('/login', (req, res) => {
+	user = req.body.name || null;
+	res.redirect('/');
+});
 // TODO: Serve up the rooms page
 app.get('/rooms', (req, res, next) => {
     // TODO
